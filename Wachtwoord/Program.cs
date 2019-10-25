f42e7e4e9b404e69d541a6e328d2e2a9d64f209d
Enrick De Munter <enrick.demunter@student.romerocollege.be>
2019-10-25 10:47:01 +0200

    commit deze info

--- Wachtwoord/Program.cs
+++ Wachtwoord/Program.cs
@@ -1,5 +1,13 @@
 ﻿//Enrick De Munter 5ITN nr4
-/*
+/*analyse
+ * vraag de eerste 2 letters van de famillienaam
+ * vraag de zonenummer van het telefoonnummer zonder de 0
+ * het eerste cijfer van de postcode
+ * 
+ * bereken wachtwoord
+ * famillienaam(de eerste twee letters en die dan omwisselen
+ * zonenummer(de zone nummer zonder nul)
+ * het eerste cijfer van de postcode en dat in het kwadraat
  * 
  * 
  */
@@ -17,7 +25,18 @@ namespace Wachtwoord
     class Program
     {
         static void Main(string[] args)
-        {
+        {   //declaraties
+            ushort lettersfamillienaam;
+            ushort zonenummer;
+            ushort postcodenummer;
+
+
+
+
+            //wachten
+            Console.WriteLine();
+            Console.WriteLine("druk op enter om af te sluiten: ");
+            Console.ReadKey();
         }
     }
 }
